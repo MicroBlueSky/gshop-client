b21d18a8ecd39d24ce8d37aee9b626162a360a61
孙宇飞 <sunyufei@hikvision.com.cn>
2021-07-28 21:41:30 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <router-view/>
+        <footer-guide v-show="$route.meta.showFooter"/>
+    </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+export default{
+    components: {
+        FooterGuide
+    }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .app 
+    color: red
+</style>
